550d9b5163a407f031ed7475af28b1422d5158b4
Berwyn Tan <berwyntan@gmail.com>
2023-04-18 23:30:46 +0800

    add styling

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,7 +3,7 @@ import type { PropsWithChildren } from "react";
 export const PageLayout = (props: PropsWithChildren) => {
   return (
     <main className="flex h-screen justify-center">
-      <div className="xs:p-6 h-full w-full border-slate-400 p-1 lg:max-w-5xl bg-slate-100">
+      <div className="xs:p-6 h-full w-full border-slate-400 p-1 lg:max-w-5xl">
         {props.children}
       </div>
     </main>
--- src/pages/project/[projectSlug].tsx
+++ src/pages/project/[projectSlug].tsx
@@ -2,6 +2,7 @@ import type { GetStaticProps } from "next";
 import projects from "data/projects";
 import type { projectsType } from "data/projects";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const ProjectSinglePage = (props: {
   projectSlug: string;
@@ -19,42 +20,74 @@ const ProjectSinglePage = (props: {
     githubLink,
     language,
   } = props?.project;
+
+  // const router = useRouter();
+  // console.log(router.query.projectSlug);
+  // // const projectSlug: string = router.query.projectSlug;
+  // const projectSlug: string = props.projectSlug;
+  // const project = projects?.find(
+  //   (proj) => proj.projectSlug === projectSlug
+  // );
+
+  // const {
+  //   techStack,
+  //   gif,
+  //   header,
+  //   subHeader,
+  //   desc1,
+  //   desc2,
+  //   desc3,
+  //   appLink,
+  //   githubLink,
+  //   language,
+  // } = project;
   return (
-    <div className="p-5">
-      <h1 className="my-1 text-2xl font-bold">{header}</h1>
-      <h2 className="my-2 italic">{subHeader}</h2>
-      <div>
+    <div className="flex flex-col items-center p-5">
+      <div className="mb-3 flex items-end justify-between md:w-10/12">
+        <div className="flex flex-col">
+          <h1 className="my-1 text-2xl font-bold">{header}</h1>
+          <h2 className="italic">{subHeader}</h2>
+        </div>
+        <div className="ml-10 flex gap-5">
+          <div className="h-8 w-8 border-b-2 border-dotted border-gray-500 p-1">
+            <Link href={appLink} target="_blank">
+              <img
+                src="https://berwyntanv2.vercel.app/assets/newTab-600daff5.png"
+                alt="project link"
+              />
+            </Link>
+          </div>
+          <div className="h-8 w-8 border-b-2 border-dotted border-gray-500 p-1">
+            <Link href={githubLink} target="_blank">
+              <img
+                src="https://berwyntanv2.vercel.app/assets/github-746a99c7.png"
+                alt="github link"
+              />
+            </Link>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex justify-center">
         <img src={gif} alt={header} />
       </div>
-      <p className="my-2">{desc1}</p>
-      <p className="my-2">{desc2}</p>
-      <p className="my-2">{desc3}</p>
 
-      <div className="my-1 flex">
-        <p className="mr-1 italic">Language:</p>
-        <p className="">{language}</p>
-      </div>
-
-      <div className="my-1 flex flex-col">
-        <p className="mr-1 italic">Tech stack:</p>
-        <p className="">{techStack}</p>
-      </div>
-      <div className="my-4 flex gap-5">
-        <div className="h-10 w-10">
-          <Link href={appLink} target="_blank">
-            <img
-              src="https://berwyntanv2.vercel.app/assets/newTab-600daff5.png"
-              alt="project link"
-            />
-          </Link>
+      <div className="flex w-10/12 flex-col items-start">
+        <div className="my-2 mt-3 flex flex-col">
+          <p className="mr-1 font-light italic">Language:</p>
+          <p className="">{language}</p>
         </div>
-        <div className="h-10 w-10">
-          <Link href={githubLink} target="_blank">
-            <img
-              src="https://berwyntanv2.vercel.app/assets/github-746a99c7.png"
-              alt="github link"
-            />
-          </Link>
+
+        <div className="my-2 flex flex-col">
+          <p className="mr-1 font-light italic">Tech stack:</p>
+          <p className="">{techStack}</p>
+        </div>
+
+        <div className="my-2 flex flex-col">
+          <p className="mr-1 font-light italic">What&apos;s it about:</p>
+          <p className="mb-1">{desc1}</p>
+          <p className="mb-1">{desc2}</p>
+          <p className="mb-4">{desc3}</p>
         </div>
       </div>
     </div>
@@ -63,6 +96,7 @@ const ProjectSinglePage = (props: {
 
 export const getStaticProps: GetStaticProps = (context) => {
   const projectSlug = context.params?.projectSlug;
+  // to fetch projects json from a DB
   const project = projects?.find((proj) => proj.projectSlug === projectSlug);
 
   return {
